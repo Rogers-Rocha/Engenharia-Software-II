0f1e70829a4c6f81198e59cb79dcc4f8df49a696
Mal-Ourisso <mauricio.pinheiro@ufpi.edu.br>
2026-05-26 17:37:41 -0300

    Criado módulo de opções do menu principal

--- Projeto-Final/Front-End/src/App.jsx
+++ Projeto-Final/Front-End/src/App.jsx
@@ -3,6 +3,7 @@ import Header from "./Header.jsx";
 import Menu from "./Menu.jsx";
 import Home from "./Home.jsx";
 import options from "./options.json";
+import { addHandlers, objToOpt } from "./options.jsx";
 import "./App.css";
 
 function App() {
@@ -11,25 +12,6 @@ function App() {
 
   const toggleMenu = () => setMenuOpen(!menuIsOpen);
 
-  // Função para abrir/fechar folders do menu principal
-  const handleToggleFolder = (e) => {
-    e.preventDefault();
-
-    const currentFolder = e.currentTarget.closest(".menu-folder");
-    const subMenu = currentFolder.querySelector("ul");
-    const icon = currentFolder.querySelector(".support i");
-
-    currentFolder.classList.toggle("active");
-
-    if (currentFolder.classList.contains("active")) {
-      subMenu.style.display = "block";
-      icon.className = "fas fa-angle-up";
-    } else {
-      subMenu.style.display = "none";
-      icon.className = "fas fa-angle-down";
-    }
-  };
-
   // TEMPORARIO
   // IMPLEMENTAR MUDANÇA DA PÁGINA DE ACORDO COM A OPÇÃO
   const tempHandler = (e) => {
@@ -37,48 +19,7 @@ function App() {
     toggleMenu();
   };
 
-  // Adiciona o handler para cada um dos objetos
-  for (const [key, value] of Object.entries(options)) {
-    if (value.type == "item") {
-      options[key].handler = tempHandler;
-    } else if (value.type == "folder") {
-      value.handler = handleToggleFolder;
-      for (const innerKey of Object.keys(value.items)) {
-        options[key].items[innerKey].handler = tempHandler;
-      }
-    }
-  }
-
-  const expandFolder = (folder) => (
-    <ul style={{ display: "none" }}>
-      {Object.values(folder.items).map((option) => (
-        <li>
-          <a className="menu-item" href={option.href} onClick={option.handler}>
-            <span className="content">{option.name}</span>
-          </a>
-        </li>
-      ))}
-    </ul>
-  );
-
-  // Converte um objeto em uma opção para o menu principal
-  const objToOpt = (option) => {
-    return (
-      <div className="menu-folder">
-        <a className="menu-item" href={option.href} onClick={option.handler}>
-          <span className="content">{option.name}</span>
-          {option.type === "folder" ? (
-            <span className="support">
-              <i className="fas fa-angle-down"></i>
-            </span>
-          ) : (
-            <></>
-          )}
-        </a>
-        {option.type === "folder" ? expandFolder(option) : <></>}
-      </div>
-    );
-  };
+  addHandlers(options, tempHandler);
 
   const tempPesquisa = (inputPesquisa) =>
     alert(`Pesquisando ${inputPesquisa}...`);
@@ -95,7 +36,7 @@ function App() {
         isOpen={menuIsOpen}
         toggleMenu={toggleMenu}
         options={
-          <nav className="menu-body">
+          <nav className="menu-body" style={{ fontWeight: "bold" }}>
             {Object.values(options).map(objToOpt)}
           </nav>
         }
---
+++ Projeto-Final/Front-End/src/options.jsx
@@ -0,0 +1,65 @@
+import "./App.css";
+
+// Função para abrir/fechar folders do menu principal
+const handleToggleFolder = (e) => {
+  e.preventDefault();
+
+  const currentFolder = e.currentTarget.closest(".menu-folder");
+  const subMenu = currentFolder.querySelector("ul");
+  const icon = currentFolder.querySelector(".support i");
+
+  currentFolder.classList.toggle("active");
+
+  if (currentFolder.classList.contains("active")) {
+    subMenu.style.display = "block";
+    icon.className = "fas fa-angle-up";
+  } else {
+    subMenu.style.display = "none";
+    icon.className = "fas fa-angle-down";
+  }
+};
+
+// Adiciona o handler para cada um dos objetos
+export function addHandlers(options, handler) {
+  for (const [key, value] of Object.entries(options)) {
+    if (value.type == "item") {
+      options[key].handler = handler[key] ?? handler;
+    } else if (value.type == "folder") {
+      options[key].handler = handleToggleFolder;
+      for (const innerKey of Object.keys(value.items)) {
+        options[key].items[innerKey].handler = handler[key] ?? handler;
+      }
+    }
+  }
+}
+
+const expandFolder = (folder) => (
+  <ul style={{ display: "none" }}>
+    {Object.values(folder.items).map((option, index) => (
+      <li key={index}>
+        <a className="menu-item" href={option.href} onClick={option.handler}>
+          <span className="content">{option.name}</span>
+        </a>
+      </li>
+    ))}
+  </ul>
+);
+
+// Converte um objeto em uma opção para o menu principal
+export function objToOpt(option, index) {
+  return (
+    <div className="menu-folder" key={index}>
+      <a className="menu-item" href={option.href} onClick={option.handler}>
+        <span className="content">{option.name}</span>
+        {option.type === "folder" ? (
+          <span className="support">
+            <i className="fas fa-angle-down"></i>
+          </span>
+        ) : (
+          <></>
+        )}
+      </a>
+      {option.type === "folder" ? expandFolder(option) : <></>}
+    </div>
+  );
+}
