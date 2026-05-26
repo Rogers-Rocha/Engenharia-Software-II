eaaf71b2f941d47000712b90a660f74077403c1b
Mal-Ourisso <mauricio.pinheiro@ufpi.edu.br>
2026-05-26 09:13:30 -0300

    Utilização inicial do govbr-ds webcomponents

--- Projeto-Final/Front-End/src/App.jsx
+++ Projeto-Final/Front-End/src/App.jsx
@@ -1,12 +1,29 @@
-import './App.css'
-import Login from './Componentes/Login/Login';
+import "./App.css";
+import { BrHeader, BrButton, BrIcon } from "@govbr-ds/webcomponents-react";
 
 function App() {
   return (
-    <div className="App">
-      <Login/>
-    </div>
+    <header className="br-header">
+      <div className="container-lg">
+        <div className="header-top">
+          <div className="header-logo">
+            <img
+              src="https://www.gov.br/ds/assets/img/govbr-logo-large.png"
+              alt="Logo"
+            />
+            <span className="br-divider vertical mx-1"></span>
+            <div className="header-sign">Órgão Responsável</div>
+          </div>
+          <div className="header-actions">
+            <BrButton primary onBrClick={() => console.log("Login")}>
+              <BrIcon icon="user" />
+              <span className="ml-1">Entrar</span>
+            </BrButton>
+          </div>
+        </div>
+      </div>
+    </header>
   );
 }
 
-export default App
+export default App;
